f62c6907d90c5996746f2eea87f839b965503f27
rc678 <rc678@eden.rutgers.edu>
2014-04-25 02:15:33 -0400

    Initinal Commit
    Skeleton of binary search game.

---
+++ binarySearch.java
@@ -0,0 +1,36 @@
+/*This is a simple program that demonstrates binary search.
+* A number from 1 to 100 will be generated and the user must 
+* guess the number.
+*/
+
+public static void main(String[] args)
+{
+  Finch myFinch = new Finch();
+  int Low = 1;
+  int High = 100;
+  int R = r.nextInt(High-Low) + Low;
+  
+  System.out.println("ENTER NUMBER BETWEEN 1 AND 100");
+  Scanner in = new Scanner(System.in);
+  
+  while(true)
+  {
+    if(in == R) //user guesses the number correctly
+    {
+    
+    }
+    
+    if(in < R)
+    {
+    
+    Scanner in = new Scanner(System.in);  
+    }
+    
+    if(in > R)
+    {
+    
+    Scanner in = new Scanner(System.in);
+    }
+  }//end of while
+
+}//end of main
