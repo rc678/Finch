6d2e2adb25325dffce617c60ec7134efb64be1a1
rc678 <rc678@eden.rutgers.edu>
2014-04-25 02:52:29 -0400

    Functional Game
    A game in which the user must guess the number that the Finch robot has generated.

--- binarySearch.java
+++ binarySearch.java
@@ -1,36 +1,53 @@
+import java.util.*;
+
+import edu.cmu.ri.createlab.terk.robot.finch.Finch;
+
+
 /*This is a simple program that demonstrates binary search.
-* A number from 1 to 100 will be generated and the user must 
-* guess the number.
-*/
+ * A number from 1 to 100 will be generated and the user must 
+ * guess the number.
+ */
 
-public static void main(String[] args)
-{
-  Finch myFinch = new Finch();
-  int Low = 1;
-  int High = 100;
-  int R = r.nextInt(High-Low) + Low;
-  
-  System.out.println("ENTER NUMBER BETWEEN 1 AND 100");
-  Scanner in = new Scanner(System.in);
-  
-  while(true)
-  {
-    if(in == R) //user guesses the number correctly
-    {
-    
-    }
-    
-    if(in < R)
-    {
-    
-    Scanner in = new Scanner(System.in);  
-    }
-    
-    if(in > R)
-    {
-    
-    Scanner in = new Scanner(System.in);
-    }
-  }//end of while
+public class binarySearch {
+	
+	public static void main(String[] args) {
+		Finch finch = new Finch();
+		int Low = 1;
+		int High = 100;
+		Random r = new Random();
+		int R = r.nextInt(High - Low) + Low;
 
-}//end of main
+		System.out.println("ENTER NUMBER BETWEEN 1 AND 100");
+		Scanner scanner = new Scanner(System.in);
+		int userNumber = scanner.nextInt();
+		
+		//System.out.println(R); //Prints the correct answer 
+		
+		while (true) {
+			System.out.println("in loop");
+			if ( userNumber == R) // user guesses the number correctly
+			{
+				
+				finch.saySomething("You guessed correctly!");
+				finch.setWheelVelocities(255,255,1000);
+			    finch.setWheelVelocities(-180,180,500);
+			    finch.setWheelVelocities(-255,-255,1000);;
+			    finch.setWheelVelocities(180,-180,500);
+			    finch.setWheelVelocities(255,255,1000);
+			    break;
+			}
+
+			if (userNumber < R) {
+				finch.saySomething("My number is higher than your the number you entered.");
+				userNumber = scanner.nextInt();
+			}
+
+			if (userNumber > R) {
+				finch.saySomething("My number is lower than the number you entered.");
+				userNumber = scanner.nextInt();
+			}
+		}// end of while
+
+		System.out.println("outside loop");
+	}// end of main
+}
